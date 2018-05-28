3a068e7a1c4ccd3f3b399e164f262a906186fa81
Sagar Bhokre <sagarbhokre@gmail.com>
2018-05-28 11:29:33 +0530

    New model

--- common.py
+++ common.py
@@ -7,14 +7,14 @@ input_height = 600
 input_width = 800
 image_shape = (input_height, input_width)
 
-nw_height = 304
+nw_height = 336
 nw_width = 800
 nw_shape = (nw_height, nw_width)
 
-OFFSET_HIGH=194
+OFFSET_HIGH=190
 OFFSET_LOW=OFFSET_HIGH+nw_height
 
-visualize = True
+visualize = False
 enable_profiling = False
 
-model_path = 'checkpoint/ep-009-val_loss-1.0074.hdf5'
+model_path = 'checkpoint/ep-009-val_loss-1.0040.hdf5'
